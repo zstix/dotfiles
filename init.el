00bf9be2127e4c1bf118521ba82dda3bfabc7f52
Zack Stickles <zstickles@gmail.com>
2021-05-27 11:54:02 -0700

    chore: trying to wrangle some frustrating elements

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(setq vc-follow-symlinks t)
 (org-babel-load-file
  (expand-file-name
   "config.org"
@@ -8,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yaml-mode atom-one-dark-theme helm company evil-leader dracula-theme evil-escape evil-escape-mode evil)))
+   '(undo-tree cmake-mode vterm yaml-mode atom-one-dark-theme helm company evil-leader dracula-theme evil-escape evil-escape-mode evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
