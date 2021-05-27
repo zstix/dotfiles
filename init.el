70e98968f177d9d2de93063e615fbe3b282ae8c0
Zack Stickles <zstickles@gmail.com>
2021-05-27 16:25:52 -0700

    chore: more irc config

--- init.el
+++ init.el
@@ -9,10 +9,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(undo-tree cmake-mode vterm yaml-mode atom-one-dark-theme helm company evil-leader dracula-theme evil-escape evil-escape-mode evil)))
+   '(prettier-js undo-tree cmake-mode vterm yaml-mode atom-one-dark-theme helm company evil-leader dracula-theme evil-escape evil-escape-mode evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-block ((t (:background "#1e1e2a")))))
